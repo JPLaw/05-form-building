1d4326ef6bcbbb9bb68147110aa6b2fc3091c682
Jenny Lawrence <jlawrence010@gmail.com>
2018-05-13 21:45:26 -0700

    function to create articles

--- jenny-lawrence-lab05/scripts/articleView.js
+++ jenny-lawrence-lab05/scripts/articleView.js
@@ -82,13 +82,14 @@ articleView.initNewArticlePage = () => {
 
   // TODO: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
+  $('#article-export').hide();
 
   $('#article-json').on('focus', function(){
     this.select();
   });
 
   // TODO: Add an event handler to update the preview and the export field if any inputs change.
-
+  $('#new-form').on('change', 'input', 'text', articleView.create);
 };
 
 articleView.create = () => {
@@ -97,17 +98,20 @@ articleView.create = () => {
 
 
   // TODO: Instantiate an article based on what's in the form fields:
-  let article = new Article({
-    title: $('#article-title').val(),
+  let newArticle = new Article({
     author: $('#article-author').val(),
-    publishedOn: $('article-publishedOn:checked')
+    authorUrl: $('#article-author-url').val(),
+    title: $('#article-title').val(),
+    category: $('#article-category').val(),
+    body: $('#article-body').val(),
+    publishedOn: $('article-publishedOn:checked').length ? new Date() : null,
 
   });
 
-  // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
+  // DONE: Use our interface to the Handblebars template to put this new article into the DOM:
+newArticle.toHtml();
 
-
-  // TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
+  // TODO:   Stretch Goal: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
   $('pre code').each();
 
   // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
