8d70ee8ba3c27d8745ec39ae43381535a011d98b
Jenny Lawrence <jlawrence010@gmail.com>
2018-05-13 23:41:15 -0700

    JSON achieved

--- jenny-lawrence-lab05/scripts/articleView.js
+++ jenny-lawrence-lab05/scripts/articleView.js
@@ -78,9 +78,9 @@ articleView.setTeasers = () => {
 // PUT YOUR RESPONSE HERE
 articleView.initNewArticlePage = () => {
   // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
+  $('#tab-content').show();
 
-
-  // TODO: The new articles we create will be copy/pasted into our source data file.
+  // DONE: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
   $('#article-export').hide();
 
@@ -88,16 +88,16 @@ articleView.initNewArticlePage = () => {
     this.select();
   });
 
-  // TODO: Add an event handler to update the preview and the export field if any inputs change.
+  // DONE: Add an event handler to update the preview and the export field if any inputs change.
   $('#new-form').on('change', 'input', 'text', articleView.create);
 };
 
 articleView.create = () => {
-  // TODO: Set up a variable to hold the new article we are creating.
+  // DONE: Set up a variable to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
 
 
-  // TODO: Instantiate an article based on what's in the form fields:
+  // DONE: Instantiate an article based on what's in the form fields:
   let newArticle = new Article({
     author: $('#article-author').val(),
     authorUrl: $('#article-author-url').val(),
@@ -109,12 +109,16 @@ articleView.create = () => {
   });
 
   // DONE: Use our interface to the Handblebars template to put this new article into the DOM:
-newArticle.toHtml();
+  newArticle.toHtml();
 
-  // TODO:   Stretch Goal: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
+  // Stretch Goal: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
   $('pre code').each();
 
   // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  $('#article-export').show();
+
+  $('#article-json').attr( 'placeholder', JSON.stringify(newArticle));
+
 
 };
 
